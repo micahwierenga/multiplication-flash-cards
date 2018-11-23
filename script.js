d59d7cd6e41af975832cd5b610b056816ab4ab11
micahwierenga <wierenga.micah@gmail.com>
2018-11-23 01:10:18 -0700

    Multiplication is functioning correctly, as is color animation and correct/incorrect answer handling

--- script.js
+++ script.js
@@ -3,24 +3,67 @@ function generateRandomNumber() {
 	return range[Math.floor( Math.random() * range.length )];
 }
 
-function createMultiplicationProblem() {
-	let multiplicand = generateRandomNumber();
-	let multiplier = generateRandomNumber();
-	let product = multiplicand * multiplier;
+function createMultiplicationProblem( multiplicand, multiplier ) {
+	let newMultiplicand = ( multiplicand !== null ) ? multiplicand : generateRandomNumber();
+	let newMultiplier = ( multiplier !== null ) ? multiplier : generateRandomNumber();
+	let product = newMultiplicand * newMultiplier;
 
-	document.getElementById( 'multiplicand' ).append( multiplicand );
-	document.getElementById( 'multiplier' ).append( multiplier );
+	document.getElementById( 'multiplicand' ).append( newMultiplicand );
+	document.getElementById( 'multiplier' ).append( newMultiplier );
 	document.getElementById( 'productInput' ).setAttribute( 'data', product );
+}
+
+function clearNumberDivsAndInput() {
+	document.getElementById( 'multiplicand' ).innerHTML = '';
+	document.getElementById( 'multiplier' ).innerHTML = '';
+	document.getElementById( 'productInput' ).value = '';
+}
+
+function colorAndFadeBackground( color ) {
+	let green = ( color === '#00FF00' ) ? 255 : 0;
+	let red = ( color === '#FF0000' ) ? 255 : 0;
+	let opacity = 1;
+	let target = document.getElementById( 'bodyBackground' );
+	target.style.background = color;
+	let fadeEffect = setInterval( function() {
+		if( !target.style.backgroundColor ) {
+			target.style.backgroundColor = "rgba( " + red + ", " + green + ", 0, " + opacity + " )";
+		}
+		if( opacity > 0 ) {
+			target.style.backgroundColor = "rgba( " + red + ", " + green + ", 0, " + opacity + " )";
+			opacity -= 0.1;
+		} else {
+			clearInterval( fadeEffect );
+		}
+	}, 50 );
 }
 
 function checkAnswer() {
 	let submittedAnswer = document.getElementById( 'productInput' ).value;
 	let correctAnswer = document.getElementById( 'productInput' ).getAttribute( 'data' );
 	if( submittedAnswer === correctAnswer ) {
-		console.log( 'Well done' );
+		// colorAndFadeBackground( '#00FF00' );
+		// clearNumberDivsAndInput();
+		// createMultiplicationProblem();
+		handleAnswer( true );
 	} else {
-		console.log( 'Try again' );
+		
+
+		// colorAndFadeBackground(  );
+		// clearNumberDivsAndInput();
+		// createMultiplicationProblem( existingMultiplicand, existingMultiplier );
+		handleAnswer( false );
 	}
+	
 }
 
-createMultiplicationProblem();
+function handleAnswer( isRight ) {
+	let color = isRight ? '#00FF00' : '#FF0000';
+	let existingMultiplicand = isRight ? null : document.getElementById( 'multiplicand' ).innerHTML;
+	let existingMultiplier = isRight ? null : document.getElementById( 'multiplier' ).innerHTML;
+	colorAndFadeBackground( color );
+	clearNumberDivsAndInput();
+	createMultiplicationProblem( existingMultiplicand, existingMultiplier );
+}
+
+createMultiplicationProblem( null, null );
